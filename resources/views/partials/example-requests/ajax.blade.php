ca774f3791426a89e4d4a7e79ce72aa324807a99
roxanariza <roxana_r2000@yahoo.com>
2019-05-29 15:01:05 +0300

    Display data in the request examples only when necessary

--- resources/views/partials/example-requests/ajax.blade.php
+++ resources/views/partials/example-requests/ajax.blade.php
@@ -2,8 +2,10 @@
 $.ajax({
     url: 'https://www.domain.com' +
              '/{{ ltrim($route['boundUri'], '/') }}',
+@if(count($route['bodyParameters']))
 {!! json_encode($route['cleanBodyParameters'], JSON_PRETTY_PRINT) !!}
    ,
+@endif
     success: function(response) {},
     error: function(response) {}
 });
